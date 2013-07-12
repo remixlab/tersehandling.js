696ef5f589554f23280490bcbbceecd181ddbba6
nakednous <nakednous@gmail.com>
2013-07-12 15:34:00 +0000

    updating tuioAgent example a bit, to make it more compact

--- examples/TuioAgent/TUIOAgent.java
+++ examples/TuioAgent/TUIOAgent.java
@@ -14,16 +14,14 @@ public class TUIOAgent
 extends
 GenericMotionAgent<GenericMotionProfile<MotionAction>, GenericClickProfile<ClickAction>>
 implements EventConstants {
-
+	
   PGraphics canvas;
   GenericDOF2Event<MotionAction> event, prevEvent;
   Map<Integer, Grabbable> grabMap = new HashMap<Integer, Grabbable>();
 
   public TUIOAgent(TerseHandler scn, String n, PGraphics canvas) {
-
     super(new GenericMotionProfile<MotionAction>(), 
     new GenericClickProfile<ClickAction>(), scn, n);
-
     this.canvas = canvas;
     // default bindings
     clickProfile().setClickBinding(TH_LEFT, 1, ClickAction.CHANGE_COLOR);
@@ -32,22 +30,18 @@ implements EventConstants {
 
   public void addTuioCursor(TuioCursor tcur) {
     event = new GenericDOF2Event<MotionAction>(prevEvent, 
-    tcur.getScreenX(canvas.width), tcur.getScreenY(canvas.height), 
-    0, 0);
+    tcur.getScreenX(canvas.width), tcur.getScreenY(canvas.height), 0, 0);
 
     updateGrabber(event);
     Grabbable grabbable = this.trackedGrabber();
 
-    if (grabbable != null) {
+    if (grabbable != null)
       grabMap.put(tcur.getCursorID(), grabbable);
-    }
   }
 
   // called when a cursor is moved
   public void updateTuioCursor(TuioCursor tcur) {
-
     Grabbable trackedGrabber = grabMap.get(tcur.getCursorID());
-
     if (trackedGrabber != null) {
       event = new GenericDOF2Event<MotionAction>(prevEvent, 
       tcur.getScreenX(canvas.width), 
@@ -57,18 +51,13 @@ implements EventConstants {
       handle(event);
       enableTracking();
     } 
-    else {
-    }
     prevEvent = event.get();
   }
 
   // called when a cursor is removed from the scene
   public void removeTuioCursor(TuioCursor tcur) {
-    event = new GenericDOF2Event<MotionAction>(prevEvent, -1000, -1000, 0, 
-    0);
-
+    event = new GenericDOF2Event<MotionAction>(prevEvent, -1000, -1000, 0, 0);
     updateGrabber(event);
-
     grabMap.remove(tcur.getCursorID());
   }
 }

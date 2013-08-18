87eb52d2b0c0b8c07c9a70da5d7503e48549cb66
nakednous <nakednous@gmail.com>
2013-08-18 16:28:11 +0000

    updating copyright note

--- src/remixlab/tersehandling/generic/agent/GenericWheeledMotionAgent.java
+++ src/remixlab/tersehandling/generic/agent/GenericWheeledMotionAgent.java
@@ -1,3 +1,27 @@
+/**
+ *                  TerseHandling (version 0.70.0)
+ *           Copyright (c) 2013 by Jean Pierre Charalambos
+ *                 @author Jean Pierre Charalambos
+ *             https://github.com/nakednous/tersehandling
+ *           
+ * This library provides classes to ease the creation of interactive scenes.
+ * 
+ * This source file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * A copy of the GNU General Public License is available on the World Wide Web
+ * at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by
+ * writing to the Free Software Foundation, 51 Franklin Street, Suite 500
+ * Boston, MA 02110-1335, USA.
+ */
+
 package remixlab.tersehandling.generic.agent;
 
 import remixlab.tersehandling.core.*;

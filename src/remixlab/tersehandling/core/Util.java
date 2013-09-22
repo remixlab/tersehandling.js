ab1f240e60dce806f507fb4dd3027f547c24d67d
nakednous <nakednous@gmail.com>
2013-09-22 10:27:03 -0500

    fixing header comment

--- src/remixlab/tersehandling/core/Util.java
+++ src/remixlab/tersehandling/core/Util.java
@@ -3,10 +3,10 @@
  * Copyright (c) 2013 Jean Pierre Charalambos.
  * @author Jean Pierre Charalambos
  * https://github.com/remixlab
- * 
+ *   
  * All rights reserved. Library that eases the creation of interactive
  * scenes released under the terms of the GNU Public License v3.0
- * which available at http://www.gnu.org/licenses/gpl.html
+ * which is available at http://www.gnu.org/licenses/gpl.html
  ******************************************************************************/
 package remixlab.tersehandling.core;
 
